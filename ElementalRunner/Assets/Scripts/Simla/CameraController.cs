c3757f78f7f8c8ef7aaa8f4f52927ab5dd492f81
Simla Beken <38881186+Simla4@users.noreply.github.com>
2022-01-19 20:39:19 +0300

    added: player jump

---
+++ ElementalRunner/Assets/Scripts/Simla/CameraController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private Vector3 offset = new Vector3(0, 7, -12);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
